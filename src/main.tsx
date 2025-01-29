c122eddaea026d6a95bf2d42065a4007d1a16465
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-01-30 00:31:24 +0600

    added login authentication for entire project

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <RouterProvider router={router} />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,42 @@
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../../redux/features/auth/authApi";
+import { useAppDispatch } from "../../redux/hooks";
+import { setUser } from "../../redux/features/auth/authSlice";
+import { verifyToken } from "../../utils/verifyToken";
+
+
 const Login = () => {
+  const dispatch = useAppDispatch();
+  const {register, handleSubmit} = useForm();
+
+  const [login, {data, error}] = useLoginMutation();
+  console.log("data :", data);
+  console.log("error :", error);
+
+  const onFormSubmit = async (data) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    }
+    const res = await login(userInfo).unwrap();
+    const token = res.data.accessToken;
+    const user = verifyToken(token);
+    console.log(user);
+    dispatch(setUser({user: user, token: token}))
+  }
+
   return (
-    <div>
-      This is Login Page;
-    </div>
+    <form onSubmit={handleSubmit(onFormSubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input type="text" id="userId" {...register("userId")}/>
+      </div>
+      <div>
+        <label htmlFor="password" >Password:</label>
+        <input type="text" id="password" {...register("password")}/>
+      </div>
+      <input type="submit" placeholder="Login" />
+    </form>
   );
 };
 
